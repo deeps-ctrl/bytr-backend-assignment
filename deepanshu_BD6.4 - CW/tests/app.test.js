9970d73bb15908de2f50af888195d63768829ba2
Deepanshu Kumar <deepskmr822@gmail.com>
2024-09-21 02:13:32 +0530

    Completed BD6.4 - CW

--- deepanshu_BD6.4 - CW/tests/app.test.js
+++ deepanshu_BD6.4 - CW/tests/app.test.js
@@ -36,9 +36,38 @@ describe('API Error Handling Test',() => {
 
     it("GET /api/books should return 404 if no books found.", async () => {
         getBooks.mockReturnValue([]);
-
         const res = await request(server).get("/api/books");
         expect(res.statusCode).toEqual(404);
-        expect(res.error).toEqual('No books found.')
+        expect(res.body.error).toEqual('No books found.')
     })
+
+    it("GET /app/books/:id should return 404 if no book found with specified id", async () => {
+        getBookById.mockReturnValue(undefined);
+
+        const res = await request(server).get("/api/books/999");
+        expect(res.statusCode).toEqual(404);
+        expect(res.body.error).toEqual("No book found.");
+    })
+
+    it("GET /api/reviews should return 404 if no review found", async () => {
+        getReviews.mockReturnValue([]);
+
+        const res = await request(server).get("/api/reviews");
+        expect(res.statusCode).toEqual(404);
+        expect(res.body.error).toEqual("No reviews found.");
+    });
+
+    it("GET /api/reviews/:id should return 404 if no review found with specified id", async () => {
+        getReviewById.mockReturnValue(undefined);
+        const res = await request(server).get("/api/reviews/999");
+        expect(res.statusCode).toEqual(404);
+        expect(res.body.error).toEqual("No review found.");
+    });
+
+    it("GET /api/users/:id should return 404 if no user found with specified id", async () => {
+        getUserById.mockReturnValue(undefined);
+        const res = await request(server).get("/api/users/999");
+        expect(res.statusCode).toEqual(404);
+        expect(res.body.error).toEqual("No user found.");
+    });
 })
